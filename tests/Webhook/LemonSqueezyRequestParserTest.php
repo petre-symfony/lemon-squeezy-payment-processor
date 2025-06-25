e0f11ca9f4a6a2f0278fb5a28dac964b6e4fb230
petrero <petrero@gmail.com>
2025-06-25 09:14:39 +0300

    13.4 Fixing the Error  - Our test is failing because we need to set up a test database. We could do this manually using Doctrine console commands, but let's take advantage of Zenstruck Foundry, which we already have installed.
     In the test class, add use ResetDatabase. This creates the test database before the first test and resets it between tests. Without this, we'd get lots of duplicate data errors. If we run the test again... it passes! Great! Now, let's write the actual test.

--- tests/Webhook/LemonSqueezyRequestParserTest.php
+++ tests/Webhook/LemonSqueezyRequestParserTest.php
@@ -3,8 +3,11 @@
 namespace App\Tests\Webhook;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Zenstruck\Foundry\Test\ResetDatabase;
 
 class LemonSqueezyRequestParserTest extends WebTestCase {
+	use ResetDatabase;
+	
 	public function testOrderCreatedWebhook(): void {
 		$client = static::createClient();
 		$crawler = $client->request('GET', '/');
