5b7142e4f65d0e0c4d8429542333634243268754
petrero <petrero@gmail.com>
2025-06-25 08:40:56 +0300

    12.3 Handling the Webhook Consumer  - Back in the LemonSqueezyWebhookConsumer::consume() method, set $payload = $event->getPayload().
     To access the user_id we set in createCheckoutUrl(), write $userId = $payload['meta']['custom_data']['user_id'] ?? null. I'll add a quick comment to explain we can't get the user by "traditional means".
    
     Now, write a sanity check with if (! $userId). If this check fails, we'll throw new InvalidArgumentException() with sprintf('User ID not found in LemonSqueezy webhook: %s', $event->getId()).
    
     We now have the $userId, but how do we get the user object? From the database! In our constructor, inject private EntityManagerInterface $entityManager.
    
     Back in the consume() method, fetch the user with $user = $this->entityManager->getRepository(User::class)->find($userId).
    
     Next, if  doesn't exist, we'll throw new EntityNotFoundException() (choose the one from Doctrine\ORM). For the message write sprintf('User "%s" not found for LemonSqueezy webhook "%s" !', $userId, $event->getId()).
    
     Below, add match ($event->getName()), and for order_created, call $this->handleOrderCreatedEvent(). This method doesn't exist yet, but we'll create it later. Also pass $event and $user as arguments. At this point, we should only have supported events, but on the off chance we're missing something, add a default that will throw new LogicException(), with sprintf('Unsupported LemonSqueezy event: %s', $event->getId()). Nice.

--- src/RemoteEvent/LemonSqueezyWebhookConsumer.php
+++ src/RemoteEvent/LemonSqueezyWebhookConsumer.php
@@ -2,16 +2,42 @@
 
 namespace App\RemoteEvent;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Component\RemoteEvent\Attribute\AsRemoteEventConsumer;
 use Symfony\Component\RemoteEvent\Consumer\ConsumerInterface;
 use Symfony\Component\RemoteEvent\RemoteEvent;
 
 #[AsRemoteEventConsumer('lemon-squeezy')]
 final class LemonSqueezyWebhookConsumer implements ConsumerInterface {
-	public function __construct() {
+	public function __construct(
+		private EntityManagerInterface $entityManager
+	) {
 	}
 
 	public function consume(RemoteEvent $event): void {
-		// Implement your own logic here
+		$payload = $event->getPayload();
+
+		//$this->getUser will not work in webhooks as no authenticated user in that processAdd comment
+		$userId = $payload['meta']['custom_data']['user_id'] ?? null;
+
+		if(!$userId){
+			throw new \InvalidArgumentException(
+				sprintf('User id not found in LemonSqueezy webhook "%s"', $event->getId())
+			);
+		}
+
+		$user = $this->entityManager->getRepository(User::class)->find($userId);
+
+		if (!$user) {
+			throw new EntityNotFoundException(
+				sprintf('User "%s" not found for LeminSqueezy webhook "%s"', $userId, $event->getId()));
+		}
+
+		match ($event->getName()) {
+			'order_created' => $this->handleOrderCreatedEvent($event, $user),
+			default => throw new \LogicException('Unsupported LemonSqueezy event: "%s"', $event->getId())
+		};
 	}
 }
