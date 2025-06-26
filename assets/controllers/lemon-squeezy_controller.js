6b037180c26fe989cbbb7625b0e73f827c45f7ad
petrero <petrero@gmail.com>
2025-06-26 13:05:49 +0300

    16.5 Back in our openOverlay() method, add e as a parameter, then call e.preventDefault() to stop JS-enabled browsers from following the link.
     For the rest of this method, grab the link element with const linkEl = e.currentTarget. Below that, we need to execute an AJAX request to the checkoutCreateUrl we passed as a value. For that, use the fetch() function for this.checkoutCreateUrlValue. For the options, add method: 'POST', and headers: {'Content-Type': 'application/json'}.
    
     Next, chain this fetch() call with .then(). Inside, expect a response, and add a sanity check - if (! response.ok), throw new Error() with Network response was not OK, followed by response.statusText.
    
     Otherwise, return response.json(). That should pass the JSON data as an object to the next .then(), where we expect data.
    
     We're going to ask LemonSqueezy to open this URL, so call window.LemonSqueezy.Url.Open() and pass data.targetUrl, which we returned from the createCheckout() action.
    
     Finally, add a catch(), expecting an error. Inside, write console.error() with a Fetch error: message, passing error as the second argument.
    
     Okay, this looks good, so let's test it out. Open our site, and also open the Developer Tools in the Console tab to see the JavaScript logs. Reload the page, and... here's our lemon-squeezy controller!
    
     If we click the "Checkout with LemonSqueezy" button, it loads and... it opens the LemonSqueezy checkout page under our domain! It still works!
    
     Again, this is subtle, so next: let's make this even cooler by rendering the checkout page over our cart page, in a modal.

--- assets/controllers/lemon-squeezy_controller.js
+++ assets/controllers/lemon-squeezy_controller.js
@@ -1,10 +1,34 @@
-import { Controler } from '@hotwired/stimulus'
+import { Controller } from '@hotwired/stimulus'
 
-export default class extends Controler {
+export default class extends Controller {
   static values = {
     checkoutCreateUrl: String
   }
 
   connect() {}
-  openOverlay(){}
+  openOverlay(e){
+    e.preventDefault()
+
+    const linkEl = e.currentTarget
+
+    fetch(this.checkoutCreateUrlValue, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if(!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText)
+        }
+
+        return response.json()
+      })
+      .then(data => {
+        window.LemonSqueezy.Url.Open(data.targetUrl)
+      })
+      .catch(error => {
+        console.error('Fetch error: ', error)
+      })
+  }
 }
