cf8d19732ef97e2e83fd7ced822b765c695f7810
petrero <petrero@gmail.com>
2025-06-26 12:47:45 +0300

    16.4 Back in our lemon-squeezy Stimulus controller, register a new value. Write static values = {}, and inside, checkoutCreateUrl: String.
     Over in the cart template, add a new data value attribute - data-lemon-squeezy-checkout-create-url-value - and pass {{ path('app_order_checkout_create') }}.
    
     I'll leave the href as-is, so, if for some reason a user doesn't have JS enabled (is that still a thing?), they can still checkout.

--- assets/controllers/lemon-squeezy_controller.js
+++ assets/controllers/lemon-squeezy_controller.js
@@ -1,6 +1,10 @@
 import { Controler } from '@hotwired/stimulus'
 
 export default class extends Controler {
+  static values = {
+    checkoutCreateUrl: String
+  }
+
   connect() {}
   openOverlay(){}
 }
